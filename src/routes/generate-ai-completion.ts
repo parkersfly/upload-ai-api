12f83ff37e117e1f863edfe1a192dcf5f01017c6
Mks <parkersfly@gmail.com>
2023-09-14 17:36:05 -0300

    generate-ai-completion route  initialized!

---
+++ src/routes/generate-ai-completion.ts
@@ -0,0 +1,23 @@
+import { FastifyInstance } from "fastify"
+import { createReadStream } from "node:fs"
+import { z } from "zod"
+import { prisma } from '../lib/prisma'
+import { openai } from '../lib/openai'
+
+export async function generateAICompletionRoute(app: FastifyInstance){
+  app.post('/ai/generate', async (req) => {
+    const bodySchema = z.object({
+      videoId: z.string().uuid(),
+      template: z.string(),
+      temperature: z.number().min(0).max(1).default(0.5),
+    })
+
+    const { videoId, template, temperature } = bodySchema.parse(req.body)
+
+    return {
+      videoId,
+      template,
+      temperature
+    }
+  })
+}
