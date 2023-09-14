161a0079eeaf19253983618626708d9f36579e12
Mks <parkersfly@gmail.com>
2023-09-14 17:21:31 -0300

    transcription route finished

--- src/routes/upload-video.ts
+++ src/routes/upload-video.ts
@@ -34,6 +34,17 @@ export async function uploadVideoRoute(app: FastifyInstance){
     const uploadDestination = path.resolve(__dirname, '../../tmp', fileUploadName)
 
     await pump(data.file, fs.createWriteStream(uploadDestination))
+
+    const video = await prisma.video.create({
+      data: {
+        name: data.filename,
+        path: uploadDestination
+      }
+    })
+
+    return {
+      video
+    }
   })
 
 }
